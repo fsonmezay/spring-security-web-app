79c19855afae2f3cd2605aa90441bae5792e0aa4
Ferdi Sönmezay <ferdi@ferdisonmezay.com>
2016-08-09 00:26:41 +0300

    create a java project with maven

---
+++ src/main/java/com/ferdisonmezay/App.java
@@ -0,0 +1,13 @@
+package com.ferdisonmezay;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
